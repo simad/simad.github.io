cc483c2a453ac4ba961f1ed74d02c1cc47ca94bd
Silvia Mandala <sil.mandala@gmail.com>
2026-04-09 23:23:44 +0200

    Remove twig

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,4 @@
-import twig from '@factorial/eleventy-plugin-twig'
 import {DateTime} from "luxon";
-
-
-const twigOptions = {
-  twig: {
-    namespaces: {},
-  },
-  images: {},
-  dir: {
-    input: "src",
-    output: "dist",
-    watch: "src/**/*.{css,js,twig,png,jpg,jpeg,gif,svg,webp}",
-  },
-};
 
 export default function (config) {
   config.addFilter('htmlDateString', dateObj => {
@@ -20,7 +6,9 @@ export default function (config) {
   })
   config.addPassthroughCopy({ 'public': './' })
   config.addPassthroughCopy("CNAME");
-  config.addPlugin(twig, twigOptions)
+  config.setNunjucksEnvironmentOptions({
+    throwOnUndefined: true,
+  });
   config.setBrowserSyncConfig({
     files: ['dist/**/*'],
     open: true,
@@ -30,6 +18,8 @@ export default function (config) {
       input: 'src',
       output: 'dist',
     },
-    markdownTemplateEngine: 'twig',
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
   }
 }
