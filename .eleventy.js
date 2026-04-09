80b20927c20060d7667ac1fd3dace90ea91efce4
Silvia Mandala <sil.mandala@gmail.com>
2026-04-09 23:37:12 +0200

    wft luxon

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,13 @@ import {DateTime} from "luxon";
 
 export default function (config) {
   config.addFilter('htmlDateString', dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat('MMMM d, yyyy')
+    const dt = DateTime.fromJSDate(dateObj);
+    const day = dt.day;
+    const suffix = day === 1 || day === 21 || day === 31 ? 'st'
+        : day === 2 || day === 22 ? 'nd'
+            : day === 3 || day === 23 ? 'rd'
+                : 'th';
+    return dt.toFormat(`MMMM d'${suffix}', yyyy`);
   })
   config.addPassthroughCopy({ 'public': './' })
   config.addPassthroughCopy("CNAME");
